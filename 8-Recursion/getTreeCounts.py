5b8c508563522c56cc76b34d6796b64b33f3cb24
Reianna Liu <128936660+ReiannaLiu@users.noreply.github.com>
2025-09-25 10:58:31 -0400

    update tree

---
+++ 8-Recursion/getTreeCounts.py
@@ -0,0 +1,22 @@
+from fc_utils import Tree
+
+def getTreeCounts(t):
+    res = [0] * 6
+    def getTreeCountsHelper(t):
+        if t.isLeaf():
+            res[t.getValue()] += 1
+        else:
+            for child in t.getChildren():
+                getTreeCountsHelper(child)
+            res[t.getValue()] += 1
+    getTreeCountsHelper(t)
+    return res
+
+def testGetTreeCounts():
+    t1 = Tree(4,
+              Tree(2),
+              Tree(1),
+              Tree(4, Tree(0)))
+    assert(getTreeCounts(t1) = [1, 1, 1, 0, 2, 0])
+
+testGetTreeCounts()
